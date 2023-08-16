313214d816747733e38ad51e992123aaaaf54d48
renan.okada <rokada@uolinc.com>
2023-08-15 21:44:48 -0300

    feat: Terceira leva d eajustes

--- src/ATV03/RedimensionarTipoP2.java
+++ src/ATV03/RedimensionarTipoP2.java
@@ -1,57 +1,44 @@
 package ATV03;
 
-import java.awt.image.BufferedImage;
-import java.io.*;
 import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class RedimensionarTipoP2 {
     public static void main(String[] args) {
-        // Caminho da imagem de entrada e saída
-        String inputFilePath = "conteudos\\Entrada_EscalaCinza.pgm";
-        String outputFilePath = "conteudos\\Entrada_EscalaCinzaAtualizada.pgm";
 
-        // Fator de escala para redimensionar
-        double scale = 0.5; // Altere o valor conforme necessário
+        String inputImagePath = "conteudos/Entrada_EscalaCinza.jpg";
+        String outputImagePath = "conteudos/Entrada_EscalaCinza2.jpg";
+
+        int newWidth = 7680;
+        int newHeight = 4320;
 
         try {
-            // Carregar a imagem de entrada
-            File inputFile = new File(inputFilePath);
-            if (!inputFile.exists()) {
-                System.out.println("Arquivo de entrada não encontrado.");
-                return;
-            }
-            BufferedImage inputImage = ImageIO.read(inputFile);
-            if (inputImage == null) {
-                System.out.println("Não foi possível carregar a imagem de entrada.");
-                return;
-            }
-            int inputWidth = inputImage.getWidth();
-            int inputHeight = inputImage.getHeight();
+            BufferedImage inputImage = ImageIO.read(new File(inputImagePath));
+            BufferedImage resizedImage = resizeImage(inputImage, newWidth, newHeight);
+            ImageIO.write(resizedImage, "jpg", new File(outputImagePath));
 
-            // Calcular a nova largura e altura
-            int outputWidth = (int) (inputWidth * scale);
-            int outputHeight = (int) (inputHeight * scale);
-
-            // Criar uma nova imagem para saída
-            BufferedImage outputImage = new BufferedImage(outputWidth, outputHeight, BufferedImage.TYPE_BYTE_GRAY);
-
-            // Redimensionar a imagem manualmente
-            for (int y = 0; y < outputHeight; y++) {
-                for (int x = 0; x < outputWidth; x++) {
-                    int inputX = (int) (x / scale);
-                    int inputY = (int) (y / scale);
-                    int grayValue = inputImage.getRGB(inputX, inputY) & 0xFF;
-                    int newPixelValue = (grayValue << 16) | (grayValue << 8) | grayValue;
-                    outputImage.setRGB(x, y, newPixelValue);
-                }
-            }
-
-            // Salvar a imagem redimensionada
-            ImageIO.write(outputImage, "PGM", new File(outputFilePath));
-
-            System.out.println("Imagem redimensionada com sucesso!");
+            System.out.println("Imagem redimensionada e salva com sucesso!");
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static BufferedImage resizeImage(BufferedImage originalImage, int newWidth, int newHeight) {
+        BufferedImage resizedImage = new BufferedImage(newWidth, newHeight, BufferedImage.TYPE_BYTE_GRAY);
+        double xRatio = (double) originalImage.getWidth() / newWidth;
+        double yRatio = (double) originalImage.getHeight() / newHeight;
+
+        for (int y = 0; y < newHeight; y++) {
+            for (int x = 0; x < newWidth; x++) {
+                int px = (int) (x * xRatio);
+                int py = (int) (y * yRatio);
+                int pixel = originalImage.getRGB(px, py);
+                resizedImage.setRGB(x, y, pixel);
+            }
+        }
+
+        return resizedImage;
     }
 }
