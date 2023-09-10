6142fcab60d1e96653a31162bbd111fa3266960e
renan.okada <rokada@uolinc.com>
2023-09-10 19:51:16 -0300

    Entrega ATV5-2

--- src/ATV05/main5b.java
+++ src/ATV05/main5b.java
@@ -2,24 +2,31 @@ package ATV05;
 
 import java.io.*;
 
-public class main {
+public class main5b {
     public static void main(String[] args) {
 
         String inputImagePath = "conteudos/EntradaEscalaCinza.pgm";
-        String inputImagePathSaida = "conteudos/EntradaEscalaCinza_saida.pgm";
+        String inputImagePathSaida = "conteudos/EntradaEscalaCinza_saida5b.pgm";
         StringWriter writer = new StringWriter();
+        Double limiar = 128.00;
 
         try (BufferedReader reader = new BufferedReader(new FileReader(inputImagePath))) {
             String line;
             int cont = 0;
             while ((line = reader.readLine()) != null) {
-                if(line.startsWith("255") && cont == 2){
-                    line = "31";
+                if(cont > 2){
+                    if(Double.parseDouble(line) <= limiar){
+                        line = "0";
+                    } else{
+                        line = "1";
+                    }
+                } else{
+                    if(line.equalsIgnoreCase("P2")){
+                        line = "P1";
+                    } else if(line.equalsIgnoreCase("255")){
+                        line = "1";
+                    }
                 }
-                if(cont >2){
-                    line = String.valueOf(Double.parseDouble(line) * 31 /255 *1.2);
-                }
-
                 writer.write(line);
                 writer.write("\n");
                 cont++;
