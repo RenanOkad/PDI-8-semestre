e1deb5a6fef364b8c16b7ffd026a15b9a6c99047
renan.okada <rokada@uolinc.com>
2023-09-17 20:31:23 -0300

    Entrega ATV8

---
+++ src/ATV08/main8a1.java
@@ -0,0 +1,45 @@
+package ATV08;
+
+import java.io.*;
+
+public class main8a1 {
+    public static void main(String[] args) {
+
+        String inputImagePath = "conteudos/Fig4.ppm";
+        String inputImagePathSaida = "conteudos/Fig4-A)imagem1.ppm";
+        StringWriter writer = new StringWriter();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputImagePath))) {
+            String line;
+            int cont = 0;
+            while ((line = reader.readLine()) != null) {
+                StringBuilder newLine = new StringBuilder();
+                if (cont > 5  && !line.equalsIgnoreCase(" ")){
+                    String[] s = line.split(" ");
+                    for(int i = 0; i <= s.length - 1; i +=3){
+                        String r = (s[i]);
+                        String g = " 0 ";
+                        String b = "0";
+                        String i1 = (r + g + b);
+                        newLine.append(i1).append(" ");
+                    }
+                    line = newLine.toString();
+                }
+                writer.write(line);
+                writer.write("\n");
+                cont++;
+            }
+
+        } catch (IOException  e) {
+            e.printStackTrace();
+        }
+
+        try (FileWriter fileWriter = new FileWriter(inputImagePathSaida)) {
+            fileWriter.write(writer.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
---
+++ src/ATV08/main8a6.java
@@ -0,0 +1,45 @@
+package ATV08;
+
+import java.io.*;
+
+public class main8a6 {
+    public static void main(String[] args) {
+
+        String inputImagePath = "conteudos/Fig4.ppm";
+        String inputImagePathSaida = "conteudos/Fig4-F)imagem6.ppm";
+        StringWriter writer = new StringWriter();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(inputImagePath))) {
+            String line;
+            int cont = 0;
+            while ((line = reader.readLine()) != null) {
+                StringBuilder newLine = new StringBuilder();
+                if (cont > 5  && !line.equalsIgnoreCase(" ")){
+                    String[] s = line.split(" ");
+                    for(int i = 0; i <= s.length - 1; i +=3){
+                        String r = " 255 ";
+                        String g = " 255 ";
+                        String b = (s[i+2]);
+                        String i1 = (r + g + b);
+                        newLine.append(i1).append(" ");
+                    }
+                    line = newLine.toString();
+                }
+                writer.write(line);
+                writer.write("\n");
+                cont++;
+            }
+
+        } catch (IOException  e) {
+            e.printStackTrace();
+        }
+
+        try (FileWriter fileWriter = new FileWriter(inputImagePathSaida)) {
+            fileWriter.write(writer.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
